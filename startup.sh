fa25dae5e228095b1f5337d15a006ee96e241d97
SUN, PEI-YUAN <404657164@gms.tku.edu.tw>
2019-08-08 21:47:00 +0800

    linux部署腳本

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
-apt update && apt upgrade -y
+apt update && sudo apt upgrade -y
 # basic install
-apt install -y sudo git gcc cpp vim tor polipo wget curl dos2unix tesseract-ocr libtesseract-dev
+sudo apt install -y sudo htop git gcc cpp vim tor polipo wget curl dos2unix tesseract-ocr libtesseract-dev
 
 
 sudo apt update; sudo apt install -y --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
@@ -8,13 +8,16 @@ sudo apt update; sudo apt install -y --no-install-recommends make build-essentia
 
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 
-echo 'export PYENV_ROOT="/home/${TARGET_USER}/.pyenv"' >> ~/.bashrc
+echo 'export PYENV_ROOT="/home/crawler/.pyenv"' >> ~/.bashrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
 
 source .bashrc
 
+pyenv install 3.7.4
+pyenv global 3.7.4
+
+#git
+
 cp torrc /etc/tor/torrc
 cp polipo.conf /etc/polipo/polipo.conf
-
-pyenv install 3.7.4
