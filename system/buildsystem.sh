5427bd8e5addc956ec38f5ad72bee1b268ca0fda
SUN, PEI-YUAN <404657164@gms.tku.edu.tw>
2019-08-26 11:35:53 +0800

    新增安裝環境腳本

---
+++ system/buildsystem.sh
@@ -0,0 +1,7 @@
+pyenv install 3.7.4
+pyenv global 3.7.4
+pip install --upgrade pip 
+pip install virtualenv
+
+cp torrc /etc/tor/torrc
+cp polipo.conf /etc/polipo/config
--- system/installpython.sh
+++ system/installpython.sh
@@ -4,8 +4,8 @@
 
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 
-echo '# pyenv init' >> ~/.bashrc
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PYENV_ROOT="/home/crawler/.pyenv"' >> ~/.bashrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
 
+source ~/.bashrc
